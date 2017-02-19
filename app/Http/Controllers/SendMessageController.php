963038bfa1779fd0b335bf9e286bc4b13cab26e0
PavelZ <grosesdorf@outlook.com>
2017-02-19 17:59:31 +0200

    edited SendMessage

--- app/Http/Controllers/SendMessageController.php
+++ app/Http/Controllers/SendMessageController.php
@@ -69,7 +69,7 @@ class SendMessageController extends Controller
       // dd($request->all());
       $arrTwilio = [];
 
-      $toPhone = '+38'.$request->input("toPhone");
+      $toPhone = '+1'.$request->input("toPhone");
       $fromUser = $request->input("fromUser");
       $textMsg = $request->input("textMsg");
       $urlImage = $request->input("urlImage");
@@ -84,36 +84,21 @@ class SendMessageController extends Controller
                     'mediaUrl' => $urlImage
                     ];
 
-      // for ($i = 1; $i <= count($urlImage); $i++) {
-      //   $arrTwilio += ['mediaUrl'.$i => $urlImage[$i-1]];
-      // }
-
-      // $arrTwilio += ['mediaUrl' => $urlImage];
-              
-
       $sms = new Client($tssid, $tstoken);
       // Use the client to do fun stuff like send text messages!
       $sms->messages->create(
           // the number you'd like to send the message to
           $toPhone,
           $arrTwilio
-          // array(
-          //     'from' => $from,
-          //     'body' => "From Pavel",
-          //     'mediaUrl' => ['https://s3-us-west-2.amazonaws.com/bucket-attack/horses/sheet2square2.jpeg',
-          //                    'https://s3-us-west-2.amazonaws.com/bucket-attack/horses/sheet2square3.jpeg' 
-          //                   ] 
-          // )
       );
 
-      
-      echo "<pre>";
-      var_dump($arrTwilio);
-      echo "</pre>";
+      // echo "<pre>";
+      // var_dump($arrTwilio);
+      // echo "</pre>";
 
       // return redirect('');
         // return redirect()->back()->with('success', 'Cheers! Your message has been sent!');
-        // return redirect()->with('success', 'Cheers! Your message has been sent!');
+        return redirect()->with('success', 'Cheers! Your message has been sent!');
 
     }
 }
